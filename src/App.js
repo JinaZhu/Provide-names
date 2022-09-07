a6b328783c59c9d81915abd983fc4461b5fafdb7
Nancy Ma <nancy.ma@getprovide.com>
2022-09-07 10:31:50 -0700

    Create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Teams from "./components/teams/teams";
+import Board from "./components/board/board";
 
 function App() {
   return (
     <div className="App">
-      <h1>Provide Names</h1>
+      <Routes>
+        <Route path='/' element={<Teams />} />
+        <Route path='/board' element={<Board />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/board/board.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Board = () => (
+  <div>
+    <h2>TIME TO PLAY!</h2>
+  </div>
+);
+
+export default Board;
